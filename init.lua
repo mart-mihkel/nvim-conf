e1d5cad462860455246958193834587e49062aef
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-03-08 08:23:51 +0200

    truncate telescope seach 🔭

--- init.lua
+++ init.lua
@@ -144,17 +144,10 @@ require("lazy").setup({
 		},
 		config = function()
 			-- [[ Configure Telescope ]]
-			-- See `:help telescope` and `:help telescope.setup()`
 			require("telescope").setup({
-				-- You can put your default mappings / updates / etc. in here
-				--  All the info you're looking for is in `:help telescope.setup()`
-				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
-				-- pickers = {}
+				defaults = {
+					path_display = { "truncate" },
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
