b9a37d88223b53629cd4e68896c21adbf93678b3
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-03-31 11:44:05 +0300

    segregate autocommand 🚗

--- init.lua
+++ init.lua
@@ -1,15 +1,6 @@
 require("options")
 require("keymaps")
-
--- [[ Basic Autocommands ]]
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
+require("autocommands")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 
