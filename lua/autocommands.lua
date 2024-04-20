b139aacfe0fe5f97e0537690cdd9b01c85e1d03a
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-04-20 13:38:44 +0300

    ➖💬

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,5 +1,3 @@
--- [[ Basic Autocommands ]]
-
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
