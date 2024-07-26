b0fc8eb69aebd243aa1375c7d51c3c4c5d83e6c7
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-26 18:46:01 +0300

    plugins💤

--- lua/config/lazy/mini.lua
+++ lua/config/lazy/mini.lua
@@ -1,10 +1,7 @@
 return {
 	"echasnovski/mini.nvim",
 	config = function()
-		-- Better Around/Inside textobjects
 		require("mini.ai").setup({ n_lines = 500 })
-
-		-- Add/delete/replace surroundings
 		require("mini.surround").setup()
 	end,
 }
