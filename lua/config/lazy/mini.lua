7943a3c0da459bdd4a31205d13f1299a103302f9
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-29 17:33:44 +0300

    mini plugins🦐

--- lua/config/lazy/mini.lua
+++ lua/config/lazy/mini.lua
@@ -1,7 +1,10 @@
 return {
 	"echasnovski/mini.nvim",
 	config = function()
-		require("mini.ai").setup({ n_lines = 500 })
+		require("mini.ai").setup({ n_lines = 512 })
+		require("mini.diff").setup({ view = { style = "sign", signs = { add = "+", change = "~", delete = "-" } } })
+		require("mini.pairs").setup()
+		require("mini.comment").setup()
 		require("mini.surround").setup()
 	end,
 }
