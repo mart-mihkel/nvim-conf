f8d2512476338e6505bdd4491e3d28d19ba78f73
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-08-06 12:35:37 +0300

    high contrast theme🥶

--- lua/config/lazy/colors.lua
+++ lua/config/lazy/colors.lua
@@ -3,22 +3,22 @@ return {
 	lazy = false,
 	priority = 1024,
 	config = function()
-		local black = { base = "#1f2328", bright = "#e0e2ea" }
-		local white = { bg = "#ffffff", fg = "#ffffff" }
+		local black = { base = "#000000", bright = "#1f2328" }
+		local telescope_border = { bg = "#e2e8f0", fg = "#e2e8f0" }
 
 		require("github-theme").setup({
 			options = { hide_end_of_buffer = false },
 			palettes = {
-				github_light = {
+				github_light_high_contrast = {
 					blue = black,
 				},
 			},
 		})
 
-		vim.api.nvim_set_hl(0, "TelescopePromptBorder", white)
-		vim.api.nvim_set_hl(0, "TelescopeResultsBorder", white)
-		vim.api.nvim_set_hl(0, "TelescopePreviewBorder", white)
+		vim.api.nvim_set_hl(0, "TelescopePromptBorder", telescope_border)
+		vim.api.nvim_set_hl(0, "TelescopeResultsBorder", telescope_border)
+		vim.api.nvim_set_hl(0, "TelescopePreviewBorder", telescope_border)
 
-		vim.cmd("colorscheme github_light")
+		vim.cmd("colorscheme github_light_high_contrast")
 	end,
 }
